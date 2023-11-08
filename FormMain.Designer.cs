019bffdeafbc13e7605c130d2bf3c0641cedc39e
JomarLozano <jomarlozano@yahoo.com>
2023-11-08 19:38:31 -0400

    ola

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -34,6 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Violet;
             this.ClientSize = new System.Drawing.Size(1207, 659);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormMain";
